9614640d4587afa262816f1e36ff4ed430449db1
Gabriel <rabeloglr@gmail.com>
2023-05-21 12:25:48 -0300

    Initial commit

---
+++ src/main/java/com/gabrielima/examplespringboothtmx/ExampleSpringBootHtmxApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielima.examplespringboothtmx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringBootHtmxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleSpringBootHtmxApplication.class, args);
+    }
+
+}
